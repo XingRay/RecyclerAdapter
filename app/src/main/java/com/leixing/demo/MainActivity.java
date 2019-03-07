3e798eae1a8fab4d6585ed4b6c2acf9f4d53bc64
leixing1012@gmail.com <wsad7206042457>
2019-03-07 17:36:47 +0800

    完成基本功能

--- app/src/main/java/com/leixing/demo/MainActivity.java
+++ app/src/main/java/com/leixing/demo/MainActivity.java
@@ -1,13 +1,31 @@
 package com.leixing.demo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btRecyclerAdapter;
+    private Activity mActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mActivity = this;
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        btRecyclerAdapter = findViewById(R.id.bt_recycler_adapter);
+        btRecyclerAdapter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecyclerAdapterActivity.start(mActivity);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/leixing/demo/RecyclerAdapterActivity.java
@@ -0,0 +1,115 @@
+package com.leixing.demo;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.leixing.recycleradapter.BaseViewHolderFactory;
+import com.leixing.recycleradapter.OnItemClickListener;
+import com.leixing.recycleradapter.RecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * description : xxx
+ *
+ * @author : leixing
+ * email : leixing1012@qq.com
+ * @date : 2019/3/7 16:58
+ */
+public class RecyclerAdapterActivity extends Activity {
+
+    public static final int SIZE = 20;
+    private Activity mActivity;
+    private Context mContext;
+    private RecyclerView rvList;
+    private RecyclerAdapter<Banner, BannerViewHolder> mAdapter;
+
+    private static int[] IMG_RES_ID = new int[]{
+            R.mipmap.img_01,
+            R.mipmap.img_02,
+            R.mipmap.img_03,
+            R.mipmap.img_04,
+            R.mipmap.img_05
+    };
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, RecyclerAdapterActivity.class);
+        if (!(context instanceof Activity)) {
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        }
+        context.startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+        mContext = getApplicationContext();
+
+        setContentView(R.layout.activity_recycler_adapter);
+
+        initView();
+        loadData();
+    }
+
+    private void initView() {
+        rvList = findViewById(R.id.rv_list);
+
+        rvList.setLayoutManager(new LinearLayoutManager(mContext));
+        mAdapter = new RecyclerAdapter<Banner, BannerViewHolder>(mContext)
+                .itemLayoutId(R.layout.view_banner_list_item)
+                .viewHolderFactory(new BaseViewHolderFactory<BannerViewHolder>() {
+                    @Override
+                    public BannerViewHolder createViewHolder(View itemView, int viewType) {
+                        return new BannerViewHolder(itemView);
+                    }
+                })
+                .itemClickListener(new OnItemClickListener<Banner>() {
+                    @Override
+                    public void onItemClick(ViewGroup parent, int position, Banner banner) {
+                        onBannerClick(position, banner);
+                    }
+                });
+
+        rvList.setAdapter(mAdapter);
+    }
+
+    private void onBannerClick(int position, Banner banner) {
+        banner.setSelect(!banner.isSelect());
+        mAdapter.notifyItemChanged(position, banner.isSelect());
+    }
+
+    private void loadData() {
+        TaskExecutor.io(new Runnable() {
+            @Override
+            public void run() {
+                final List<Banner> banners = loadBanners();
+                TaskExecutor.ui(new Runnable() {
+                    @Override
+                    public void run() {
+                        showBanners(banners);
+                    }
+                });
+            }
+        });
+    }
+
+    private List<Banner> loadBanners() {
+        List<Banner> banners = new ArrayList<>();
+        for (int i = 0; i < SIZE; i++) {
+            banners.add(new Banner(IMG_RES_ID[i % IMG_RES_ID.length]));
+        }
+        return banners;
+    }
+
+    private void showBanners(List<Banner> banners) {
+        mAdapter.update(banners);
+    }
+}
